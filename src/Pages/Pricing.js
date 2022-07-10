2d52f9371395d0004f634377a4d4f3623ed7d36f
Tobbeycc1 <jayjaytobbey@gmail.com>
2022-07-10 00:01:01 +0000

    I need to sleep

---
+++ src/Pages/Pricing.js
@@ -0,0 +1,20 @@
+import Header from "../Componets/Header"
+import classes from '../CSS files/Pricing.module.css'
+
+function Pricing(params) {
+    return(
+        <div className={classes.pricingBigCon}>
+           <Header/> 
+
+           <div className={classes.pricingCon}>
+            <h1>While we distribute, <br/> <span className={classes.noBoldOnHeader}>you stay independent, focus in your music and earn money.</span></h1>
+           </div>
+
+           <div>
+            <img src=" "/>
+           </div>
+
+        </div>
+    )
+}
+export default Pricing
