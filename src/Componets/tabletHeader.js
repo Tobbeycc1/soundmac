321ec8f5fb9be9181146229a8f10d6874b39cbfc
Tobbeycc1 <jayjaytobbey@gmail.com>
2022-07-18 01:26:12 +0000

    promotion page v 1.1

--- src/Componets/tabletHeader.js
+++ src/Componets/tabletHeader.js
@@ -2,23 +2,30 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom'
 import classes from '../CSS files/tabletheader.module.css'
 
-//  the dropdown for navbar
+
+
+function TabletHeader(props) {
+
+    //  the dropdown for navbar
 const navJSX =  <div className={classes.tabletNavBar}>
 
-<Link className={classes.nav} to={'/'}>Home</Link>
-<Link to={"/musicDistribution"}className={classes.nav}>Music Distribution</Link>
-<Link className={classes.nav} to={'/promotion'}>Promotion</Link>
-<Link className={classes.nav} to={'/pricing'}>Pricing</Link>
-<Link className={classes.nav} to={'/contact'}>Contact</Link>
-<Link className={classes.button} to={'/myportal'}>My Portal</Link>
+<Link onClick={noNavItem} className={classes.nav} to={'/'}>Home</Link>
+<Link onClick={noNavItem} to={"/musicDistribution"}className={classes.nav}>Music Distribution</Link>
+<Link onClick={noNavItem} className={classes.nav} to={'/promotion'}>Promotion</Link>
+<Link onClick={noNavItem} className={classes.nav} to={'/pricing'}>Pricing</Link>
+<Link onClick={noNavItem} className={classes.nav} to={'/contact'}>Contact</Link>
+<Link onClick={noNavItem} className={classes.button} to={'/myportal'}>My Portal</Link>
 
 </div>
 
-function TabletHeader(props) {
 const [showNavItem, setShowNavItem]= useState(false)
     function navItem() {
         console.log('nav items are visible');
         setShowNavItem(!showNavItem)
+    }
+    function noNavItem() {
+        console.log('nav items are not visible');
+        setShowNavItem(false)
     }
 
     return(
