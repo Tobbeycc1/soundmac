702c9acf4d685bce1b5b72ef45b02d90c577445b
Tobbeycc1 <jayjaytobbey@gmail.com>
2022-07-20 21:29:06 +0000

    fixed media query for 360px and below

--- src/Componets/MusicDistribution.js
+++ src/Componets/MusicDistribution.js
@@ -6,7 +6,7 @@ function MusicDistribution(props) {
 <h3 className={classes.earn}>Earn While They Listen, With Over <span className={classes.red}>2million</span> Listeners </h3>
                 <img data-aos="fade-up" data-aos-delay="50"
     data-aos-duration="1000"
-    data-aos-easing="ease-in-out" src={musicAppLogos} alt='music app logos'/>
+    data-aos-easing="ease-in-out" src={musicAppLogos} alt='music app logos' className={classes.musicAppLogos}/>
                 <p data-aos="fade-up" data-aos-delay="50"
     data-aos-duration="1000"
     data-aos-easing="ease-in-out" className={classes.musicLogotext}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse turpis lorem, placerat non purus sit amet,<br/>
