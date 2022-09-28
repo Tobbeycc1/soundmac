9ca4ddaa4781468e8bc4deabad0c60bb15518d7a
Tobbeycc1 <jayjaytobbey@gmail.com>
2022-09-28 14:10:13 +0000

    pushed to Arnold

--- src/Componets/Header.js
+++ src/Componets/Header.js
@@ -1,65 +1,66 @@
 import { useState } from "react";
-import { Link } from "react-router-dom"
-import classes from '../CSS files/Header.module.css'
+import { Link } from "react-router-dom";
+import classes from "../CSS files/Header.module.css";
 import ModalBackground from "./ModalBackground";
 import SignInBox from "./SignInBox";
 import TabletHeader from "./tabletHeader";
 import SignUp from "./SignUp";
 
-
 function Header(props) {
+  const [modal, showModal] = useState(false);
 
-    const [modal, showModal]=useState(false)
+  const [signUpModal, showSignupModal] = useState(false);
 
-    const [signUpModal, showSignupModal]= useState(false)
+  function showDeets() {
+    console.log("Login shown");
+    showModal(!modal);
+  }
+  function signUp() {
+    console.log("signUp shown");
+    showSignupModal(!signUpModal);
+    showModal(false);
+  }
 
-     function showDeets() {
-        console.log('Login shown');
-        showModal(!modal)
-        
-    }
-    function signUp() {
-        console.log('signUp shown');
-        showSignupModal(!signUpModal)
-        showModal(false)
-        
-    }
-   
-    function sDAndSM() {
-        showModal(true)
-        showSignupModal(false)
+  function sDAndSM() {
+    showModal(true);
+    showSignupModal(false);
+  }
 
-    }
+  return (
+    <div className={classes.headerContainer}>
+      <div className={classes.logoDiv}>
+        <Link className={classes.logo} to={"/"}>
+          SoundMac
+        </Link>
+      </div>
 
-    return(
-        <div className={classes.headerContainer} >
-            <div className={classes.logoDiv} >
-            <Link className={classes.logo} to={'/'}>SoundMac</Link>
-            </div>
+      <div className={classes.navDiv}>
+        <Link className={classes.nav} to={"/"}>
+          Home
+        </Link>
+        {/* <Link to={"/musicDistribution"}className={classes.nav}>Music Distribution</Link> */}
+        <Link className={classes.nav} to={"/promotion"}>
+          Promotion
+        </Link>
+        <Link className={classes.nav} to={"/pricing"}>
+          Pricing
+        </Link>
+        <Link className={classes.nav} to={"/contact"}>
+          Contact
+        </Link>
+        <span className={classes.button} onClick={showDeets}>
+          My Portal
+        </span>
+      </div>
 
-            <div className={classes.navDiv}>
-            <Link className={classes.nav} to={'/'}>Home</Link>
-            {/* <Link to={"/musicDistribution"}className={classes.nav}>Music Distribution</Link> */}
-            <Link className={classes.nav} to={'/promotion'}>Promotion</Link>
-            <Link className={classes.nav} to={'/pricing'}>Pricing</Link>
-            <Link className={classes.nav} to={'/contact'}>Contact</Link>
-            <span className={classes.button}  onClick={showDeets}>My Portal</span>
-            
-            </div>
+      <div></div>
 
-            {modal && <ModalBackground noDeets={showDeets} /> }
-            {modal && <SignInBox showDeets={showDeets} signUpAt={signUp}/> }
-           
-            {signUpModal && <ModalBackground noDeets={signUp} /> }
-            {signUpModal && <SignUp noSignUp={signUp} iHaveAnAcc={sDAndSM}/> }
-            
+      {modal && <ModalBackground noDeets={showDeets} />}
+      {modal && <SignInBox showDeets={showDeets} signUpAt={signUp} />}
 
-        </div>
-
-        
-        
-            
-           
-    )
+      {signUpModal && <ModalBackground noDeets={signUp} />}
+      {signUpModal && <SignUp noSignUp={signUp} iHaveAnAcc={sDAndSM} />}
+    </div>
+  );
 }
-export default Header
+export default Header;
