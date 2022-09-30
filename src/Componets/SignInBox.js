9bc776b1ee8fa2865833eb5b6535679bfeb6a16f
Arnold Emmanuel <arnoldemmanuel15@gmail.com>
2022-09-30 12:09:39 +0100

    Merge branch 'master' of https://github.com/Tobbeycc1/soundmac

--- src/Componets/SignInBox.js
+++ src/Componets/SignInBox.js
@@ -1,109 +1,137 @@
-import { useState, useContext } from 'react'
-import classes from '../CSS files/SignInBox.module.css'
+import { useState, useContext } from "react";
+import classes from "../CSS files/SignInBox.module.css";
 import { ImCancelCircle } from "react-icons/im";
 import { MdOutlineAttachEmail } from "react-icons/md";
-import { RiLockPasswordFill } from "react-icons/ri";
+import { RiLockPasswordLine } from "react-icons/ri";
 import { FcGoogle } from "react-icons/fc";
 import { RiFacebookCircleFill } from "react-icons/ri";
-import AuthContext from '../contexts/auth/authContext';
+import AuthContext from "../contexts/auth/authContext";
 
-import InputSignIn from './inputSignIn';
-
-
+import InputSignIn from "./inputSignIn";
+import { useEffect } from "react";
 
 function SignInBox(props) {
-    const authContext = useContext(AuthContext)
+  const authContext = useContext(AuthContext);
 
-    const { registerUser } = authContext
+  const { loginUser } = authContext;
 
-    const [userDetails, setUserDetails] = useState({
-        email: '',
-        password: '',
-    })
+  const [userDetails, setUserDetails] = useState({
+    email: null,
+    password: null,
+  });
 
-    const { email, password } = userDetails
+  const { email, password } = userDetails;
 
-    const onChange = (e) => {
-        setUserDetails((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  // if email field is empty
+  const [emailIsEmpty, setEmailIsEmpty] = useState(null);
+
+  // if password field is empty
+  const [passwordIsEmpty, setPasswordIsEmpty] = useState(null);
+
+  const onChange = (e) => {
+    setUserDetails((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    setEmailIsEmpty(false);
+    setPasswordIsEmpty(false);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    email == "" || email == null
+      ? setEmailIsEmpty(true)
+      : setEmailIsEmpty(false);
+
+    password == "" || password == null
+      ? setPasswordIsEmpty(true)
+      : setPasswordIsEmpty(false);
+
+    if (
+      email === "" ||
+      (email === null && password === "") ||
+      password === null
+    ) {
+      console.log("fields are empty");
+      // add alert(error) message
+    } else {
+      const user = {
+        email,
+        password,
+      };
+      loginUser(user);
+      console.log(user);
     }
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (email === '' && password === '') {
-            console.log('passwords do not match')
-            // add alert(error) message
-        } else {
-            const user = {
-                username: email, password
-            }
-            registerUser(user)
-            console.log(user)
-        }
-    }
-
-
-    return (
-        <div className={classes.SignInBoxDiv} >
-            <div className={`${classes.SignInBox} animate__animated animate__rubberBand`} >
-
-                {/* Cancel Button */}
-                <div className={classes.x}>
-                    <ImCancelCircle className={classes.Gi} onClick={props.showDeets} />
-                </div>
-                <p className={classes.welcomeTo}>Welcome to <span className={classes.soundmac}>SOUNDMAC</span></p>
-
-                <form onSubmit={onSubmit}>
-                    <div className={classes.inputCon}>
-
-                        <InputSignIn
-                            icon={<MdOutlineAttachEmail />}
-                            type={'email'}
-                            placeholder={'Email'}
-                            name="email"
-                            value={email}
-                            onChange={onChange}
-                        />
-
-                        <InputSignIn
-                            icon={<RiLockPasswordFill />}
-                            type={'password'}
-                            placeholder={'Password'}
-                            name="password"
-                            value={password}
-                            onChange={onChange}
-                        />
-
-                        <div className={classes.flexCon}>
-                            <div className={classes.checkBoxCon}>
-                                <input type='checkbox' className={classes.largerCheckbox} /><p className={classes.rememberMe}>Remember Me?</p>
-                            </div>
-
-                            <p className={classes.rememberMe}>Forgot Password?</p>
-                        </div>
-
-
-                    </div>
-
-                    <div className={classes.signInCon}
-                        onClick={onSubmit}
-                    >
-                        <button type='submit' className={classes.signinButton}>Sign In</button>
-
-                    </div>
-
-
-                    <p onClick={props.signUpAt} className={classes.dontHave}>Don't have an account Yet?</p>
-                </form>
-
-
-                <p className={classes.or}>or</p>
-
-                <FcGoogle className={classes.googleLoginIcon} />
-                <RiFacebookCircleFill className={classes.googleLoginIcon} />
-
-
-            </div>
+  return (
+    <div className={classes.SignInBoxDiv}>
+      <div
+        className={`${classes.SignInBox} animate__animated animate__rubberBand`}
+      >
+        {/* Cancel Button */}
+        <div className={classes.x}>
+          <ImCancelCircle className={classes.Gi} onClick={props.showDeets} />
         </div>
-    )
+        <p className={classes.welcomeTo}>
+          Welcome to <span className={classes.soundmac}>SOUNDMAC</span>
+        </p>
+
+        <form onSubmit={onSubmit}>
+          <div className={classes.inputCon}>
+            {userDetails.email == "" || emailIsEmpty == true ? (
+              <p className={classes.emailEmpty}>email field is empty</p>
+            ) : (
+              <></>
+            )}
+            <InputSignIn
+              icon={<MdOutlineAttachEmail />}
+              type={"email"}
+              placeholder={"Email"}
+              name="email"
+              value={email}
+              onChange={onChange}
+            />
+
+            {userDetails.password == "" || passwordIsEmpty == true ? (
+              <p className={classes.emailEmpty}>password field is empty</p>
+            ) : (
+              <></>
+            )}
+            <InputSignIn
+              icon={<RiLockPasswordLine />}
+              type={"password"}
+              placeholder={"Password"}
+              name="password"
+              value={password}
+              onChange={onChange}
+            />
+
+            <div className={classes.flexCon}>
+              <div className={classes.checkBoxCon}>
+                <input type="checkbox" className={classes.largerCheckbox} />
+                <p className={classes.rememberMe}>Remember Me?</p>
+              </div>
+
+              <p className={classes.rememberMe}>Forgot Password?</p>
+            </div>
+          </div>
+
+          <div className={classes.signInCon} onClick={onSubmit}>
+            <button type="submit" className={classes.signinButton}>
+              Sign In
+            </button>
+          </div>
+          <p className={classes.dontHavePrev}>Don't have an account?</p>
+          <p onClick={props.signUpAt} className={classes.dontHave}>
+            Register
+          </p>
+        </form>
+
+        <p className={classes.or}>or</p>
+
+        <FcGoogle className={classes.googleLoginIcon} />
+        <RiFacebookCircleFill className={classes.googleLoginIcon} />
+      </div>
+    </div>
+  );
 }
-export default SignInBox
+export default SignInBox;
--- src/Componets/SignUp.js
+++ src/Componets/SignUp.js
@@ -1,109 +1,261 @@
-import { useState, useContext } from 'react';
-import classes from '../CSS files/Signup.module.css'
+import { useState, useContext } from "react";
+import classes from "../CSS files/Signup.module.css";
 import { ImCancelCircle } from "react-icons/im";
 import { MdOutlineAttachEmail } from "react-icons/md";
-import { RiLockPasswordFill } from "react-icons/ri";
+import { MdDriveFileRenameOutline } from "react-icons/md";
+import { RiLockPasswordLine } from "react-icons/ri";
 import { FcGoogle } from "react-icons/fc";
 import { RiFacebookCircleFill } from "react-icons/ri";
-import InputSignIn from './inputSignIn';
-import AuthContext from '../contexts/auth/authContext';
+import { BsFlag } from "react-icons/bs";
+import InputSignIn from "./inputSignIn";
+import AuthContext from "../contexts/auth/authContext";
+import inputClasses from "../CSS files/inputSignIn.module.css";
+
+import {
+  CountryDropdown,
+  RegionDropdown,
+  CountryRegionData,
+} from "react-country-region-selector";
 
 function SignUp(props) {
+  const authContext = useContext(AuthContext);
 
-    const authContext = useContext(AuthContext)
+  const { registerUser } = authContext;
 
-    const { registerUser } = authContext
+  const [userDetails, setUserDetails] = useState({
+    first_name: null,
+    last_name: null,
+    email: null,
+    password: null,
+    confirmPassword: null,
+  });
 
-    const [userDetails, setUserDetails] = useState({
-        email: '',
-        password: '',
-        confirmPassword: ''
-    })
+  const { first_name, last_name, email, password, confirmPassword } =
+    userDetails;
 
-    const { email, password, confirmPassword } = userDetails
+  // if firstname field is empty
+  const [firstNameIsEmpty, setFirstNameIsEmpty] = useState(null);
 
-    const onChange = (e) => {
-        setUserDetails((prev) => ({ ...prev, [e.target.name]: e.target.value }))
+  // if lastname field is empty
+  const [lastNameIsEmpty, setLastNameIsEmpty] = useState(null);
+
+  // if email field is empty
+  const [emailIsEmpty, setEmailIsEmpty] = useState(null);
+
+  // if password field is empty
+  const [passwordIsEmpty, setPasswordIsEmpty] = useState(null);
+
+  // if confirmPassword field is empty
+  const [confirmPasswordIsEmpty, setConfirmPasswordIsEmpty] = useState(null);
+
+  // if country field is empty
+  const [countryIsEmpty, setCountryIsEmpty] = useState(null);
+
+  const onChange = (e) => {
+    setUserDetails((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    setEmailIsEmpty(false);
+    setPasswordIsEmpty(false);
+    setFirstNameIsEmpty(false);
+    setLastNameIsEmpty(false);
+    setConfirmPasswordIsEmpty(false);
+    setCountryIsEmpty(false);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    email == "" || email == null
+      ? setEmailIsEmpty(true)
+      : setEmailIsEmpty(false);
+
+    password == "" || password == null
+      ? setPasswordIsEmpty(true)
+      : setPasswordIsEmpty(false);
+
+    first_name == "" || first_name == null
+      ? setFirstNameIsEmpty(true)
+      : setFirstNameIsEmpty(false);
+
+    last_name == "" || last_name == null
+      ? setLastNameIsEmpty(true)
+      : setLastNameIsEmpty(false);
+
+    confirmPassword == "" || confirmPassword == null
+      ? setConfirmPasswordIsEmpty(true)
+      : setConfirmPasswordIsEmpty(false);
+
+    country == "" || country == null
+      ? setCountryIsEmpty(true)
+      : setCountryIsEmpty(false);
+
+    if (password !== confirmPassword) {
+      console.log("passwords do not match");
+      // add alert(error) message
+    } else {
+      const user = {
+        email,
+        first_name,
+        last_name,
+        country,
+        password,
+      };
+      registerUser(user);
+      console.log(user);
     }
+  };
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        if (password !== confirmPassword) {
-            console.log('passwords do not match')
-            // add alert(error) message
-        } else {
-            const user = {
-                username: email, password
-            }
-            registerUser(user)
-            console.log(user)
-        }
-    }
-    return (
-        <div className={classes.SignInBoxDiv}>
-            <div className={`${classes.SignInBox} animate__animated animate__headShake`}>
-                {/* Cancel Button */}
-                <div className={classes.x}>
-                    <ImCancelCircle className={classes.Gi} onClick={props.noSignUp} />
-                </div>
+  // For Country selcection
+  const [location, setLocation] = useState({ country: null });
+  const { country, region } = location;
 
-                <p className={classes.welcomeTo}>Thank you for choosing <span className={classes.soundmac}>SOUNDMAC</span></p>
+  const selectCountry = (val) => {
+    setLocation({ location, country: val });
+    console.log(country);
+  };
 
-                <form onSubmit={onSubmit}>
-                    <div className={classes.inputCon}>
-
-                        <InputSignIn
-                            icon={<MdOutlineAttachEmail />}
-                            type={'email'}
-                            placeholder={'Email'}
-                            name="email"
-                            onChange={onChange}
-                            value={email}
-                        />
-
-                        <InputSignIn
-                            icon={<RiLockPasswordFill />}
-                            type={'password'}
-                            placeholder={'Password'}
-                            name="password"
-                            onChange={onChange}
-                            value={password}
-                        />
-
-                        {/* Re enter password */}
-                        <InputSignIn
-                            icon={<RiLockPasswordFill />}
-                            type={'password'}
-                            placeholder={'Re-enter Password'}
-                            name="confirmPassword"
-                            onChange={onChange}
-                            value={confirmPassword}
-                        />
-
-                        <div className={classes.flexCon}>
-                            <div className={classes.checkBoxCon}>
-                                <input type='checkbox' className={classes.largerCheckbox} /><p className={classes.rememberMe}>Remember Me?</p>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div className={classes.signInCon}
-                        onClick={onSubmit}
-                    >
-                        <button type='submit' className={classes.signinButton}
-
-                        >Sign Up</button>
-                    </div>
-                    <p className={classes.dontHave} onClick={props.iHaveAnAcc}>I have an account</p>
-                </form>
-
-                <p className={classes.or}>or</p>
-
-                <FcGoogle className={classes.googleLoginIcon} />
-                <RiFacebookCircleFill className={classes.googleLoginIcon} />
-
-            </div>
+  return (
+    <div className={classes.SignInBoxDiv}>
+      <div
+        className={`${classes.SignInBox} animate__animated animate__headShake`}
+      >
+        {/* Cancel Button */}
+        <div className={classes.x}>
+          <ImCancelCircle className={classes.Gi} onClick={props.noSignUp} />
         </div>
-    )
+
+        <p className={classes.welcomeTo}>
+          Thank you for choosing{" "}
+          <span className={classes.soundmac}>SOUNDMAC</span>
+        </p>
+
+        <form onSubmit={onSubmit}>
+          <div className={classes.inputCon}>
+            {/* Input First Name */}
+            {userDetails.first_name == "" || firstNameIsEmpty == true ? (
+              <p className={classes.emailEmpty}>first name field is empty</p>
+            ) : (
+              <></>
+            )}
+            <InputSignIn
+              icon={<MdDriveFileRenameOutline />}
+              type={"name"}
+              placeholder={"First name"}
+              name="first_name"
+              onChange={onChange}
+              value={first_name}
+            />
+            {/* Input Last Name */}
+            {userDetails.last_name == "" || lastNameIsEmpty == true ? (
+              <p className={classes.emailEmpty}>last field is empty</p>
+            ) : (
+              <></>
+            )}
+            <InputSignIn
+              icon={<MdDriveFileRenameOutline />}
+              type={"name"}
+              placeholder={"Last name"}
+              name="last_name"
+              onChange={onChange}
+              value={last_name}
+            />
+            {/* input email */}
+            {userDetails.email == "" || emailIsEmpty == true ? (
+              <p className={classes.emailEmpty}>email field is empty</p>
+            ) : (
+              <></>
+            )}
+            <InputSignIn
+              icon={<MdOutlineAttachEmail />}
+              type={"email"}
+              placeholder={"Email"}
+              name="email"
+              onChange={onChange}
+              value={email}
+            />
+
+            {/* select country */}
+            {/* Input Last Name */}
+            {location.country == "" || countryIsEmpty == true ? (
+              <p className={classes.emailEmpty}>country field is empty</p>
+            ) : (
+              <></>
+            )}
+            <div className={inputClasses.formCon}>
+              <label className={inputClasses.label}>
+                <BsFlag />{" "}
+              </label>
+              <CountryDropdown
+                className={classes.Input}
+                value={country}
+                onChange={(val) => {
+                  selectCountry(val);
+                }}
+                style={{
+                  outline: "none",
+                  padding: "2px 15px",
+                  width: "100%",
+                }}
+              />
+              <br />
+            </div>
+
+            {/* input password */}
+            {userDetails.password == "" || passwordIsEmpty == true ? (
+              <p className={classes.emailEmpty}>password field is empty</p>
+            ) : (
+              <></>
+            )}
+            <InputSignIn
+              icon={<RiLockPasswordLine />}
+              type={"password"}
+              placeholder={"Password"}
+              name="password"
+              onChange={onChange}
+              value={password}
+            />
+
+            {/* confirm password */}
+
+            {password !== confirmPassword ? (
+              <p className={classes.emailEmpty}>password doesn't match</p>
+            ) : (
+              <></>
+            )}
+
+            <InputSignIn
+              icon={<RiLockPasswordLine />}
+              type={"password"}
+              placeholder={"confirmPassword"}
+              name="confirmPassword"
+              onChange={onChange}
+              value={confirmPassword}
+            />
+
+            <div className={classes.flexCon}>
+              <div className={classes.checkBoxCon}>
+                <input type="checkbox" className={classes.largerCheckbox} />
+                <p className={classes.rememberMe}>Remember Me?</p>
+              </div>
+            </div>
+          </div>
+          <div className={classes.signInCon} onClick={onSubmit}>
+            <button type="submit" className={classes.signinButton}>
+              Sign Up
+            </button>
+          </div>
+
+          <p className={classes.dontHavePrev}>Already have an account?</p>
+          <p className={classes.dontHave} onClick={props.iHaveAnAcc}>
+            Login
+          </p>
+        </form>
+
+        <p className={classes.or}>or</p>
+
+        <FcGoogle className={classes.googleLoginIcon} />
+        <RiFacebookCircleFill className={classes.googleLoginIcon} />
+      </div>
+    </div>
+  );
 }
-export default SignUp
+export default SignUp;
